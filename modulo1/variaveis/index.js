f05e248781455393db880c1452ef9e6c3e7ec595
LetoSotero <93104710+LetoSotero@users.noreply.github.com>
2021-11-09 23:37:54 -0300

    exercicio de variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,13 @@
+//exercício 1
+let nome = "Wellington"
+let idade = 34
+console.log(typeof nome)
+console.log(typeof idade)
+console.log("meu nome é: ", nome, "eu tenho ", idade, "anos")
+let primeiraPergunta = "Você torce para o Corinthians?"
+let segundaPergunta = "Você já foi ao estádio?"
+let terceiraPergunta = "O Palmeiras tem Mundial?"
+console.log(typeof primeiraPergunta)
+console.log(typeof segundaPergunta)
+console.log(typeof terceiraPegunta)
+console.log("Sim: ", primeiraPergunta, "Sim ", segundaPergunta, "O Palmeiras não tem mundial", terceiraPergunta, "NÃO")
