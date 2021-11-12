0b1be7a1f40c741060cd53e8aa7ddd41145ae1a9
LetoSotero <93104710+LetoSotero@users.noreply.github.com>
2021-11-11 21:30:47 -0300

    Entrega exercício

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,13 +1,87 @@
-//exercício 1
-let nome = "Wellington"
-let idade = 34
-console.log(typeof nome)
-console.log(typeof idade)
-console.log("meu nome é: ", nome, "eu tenho ", idade, "anos")
-let primeiraPergunta = "Você torce para o Corinthians?"
-let segundaPergunta = "Você já foi ao estádio?"
-let terceiraPergunta = "O Palmeiras tem Mundial?"
-console.log(typeof primeiraPergunta)
-console.log(typeof segundaPergunta)
-console.log(typeof terceiraPegunta)
-console.log("Sim: ", primeiraPergunta, "Sim ", segundaPergunta, "O Palmeiras não tem mundial", terceiraPergunta, "NÃO")
+/* Exercício 1
+resposta: 
+a.  false
+b.  false
+c.  true
+d.  boolean
+
+Exercício 2
+no console ira pedir para incluir um número
+
+Exercício 3
+no lugar onde está escrito "coloque um número!" adicione um número nas duas opções para que a soma possa acontecer. */
+
+
+//Exercício de escrita 1
+
+let idade=prompt("Qual sua idade")
+const primeiraIdade = 34
+let idade2=prompt("Qual a idade do seu melhor amigo?")
+const segundaIdade = 30
+let idade3=prompt("Sua idade é maior do que a do seu melhor amigo?")
+
+console.log( primeiraIdade > segundaIdade)
+let idade4=prompt("sua diferença de idade é 4")
+console.log( primeiraIdade - segundaIdade)
+
+//Exercício de escrita 2
+
+let insira=prompt("Insira o número PAR entre 4 e 10")
+let a = 10
+let b = 8
+let c = 6
+let d = 4
+let e = 2
+let f = 1
+let g = 3
+let h = 5
+let i = 7
+let j = 9
+
+
+
+let divisao = a / b
+let divisao1 = a / c
+let divisao2 = a / d
+let divisao3 = a / e
+let resto = a % 2
+let resto1 = a % 2
+let resto2 = a % 2
+let resto3 = a % 2
+
+console.log(resto)
+console.log(resto1)
+console.log(resto2)
+console.log(resto3)
+
+
+//fiz o teste com alguns numeros pares e o resto é  0
+// se o usuário inserir o número ímpar, vai aparecer 1.
+
+//Exercício de escrita 3
+
+let insira=prompt("Qual a sua idade em meses?")
+let insira2=prompt("Qual a sua idade em dias?")
+let insira3=prompt("Qual a sua idade em horas?")
+
+console.log(413)
+console.log(12.595)
+console.log(302.280)
+
+//Exercício de escrita 4
+let insira=prompt("insira dois números, 2, 4 ou 6")
+const a = 2
+const b = 4
+const c = 6
+
+const opcao1 = b > a
+const opcao2 = a === b
+const opcao3 = a === 0
+const opcao4 = b === 0
+
+
+console.log(opcao1)
+console.log(opcao2)
+console.log(opcao3)
+console.log(opcao4)
+
---
+++ modulo3/variaveis/index.js
@@ -0,0 +1,4 @@
+let a = 10
+let b = 10
+console.log(b)
+console.log(a, b)
