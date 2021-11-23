030cae2cb367988a85c5ddab16393ecf4daa72cf
LetoSotero <93104710+LetoSotero@users.noreply.github.com>
2021-11-23 19:35:33 -0300

    funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,9 @@
+const pokemons = [
+    {nome: 'Bulbasaur', tipo: 'grama'}
+    {nome: 'Charmander', tipo: 'fogo'}
+    {nome: 'Squirtle', tipo: 'agua'}
+]
+    for (const pkm of pokemons) {
+        console.log(`${pkm.nome} é de ${pkm.tipo}`)
+    }
+    
