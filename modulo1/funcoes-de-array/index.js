688a43975db9ca2666f6e0e90c0b96fcd747673b
LetoSotero <93104710+LetoSotero@users.noreply.github.com>
2021-12-02 22:36:34 -0300

    funcoes-de-array

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,36 @@
+
+
+  //----------Exercício de interpretação
+  //1. a) No console será impresso o Apelido, Nome e a posição.
+
+  //2. a) No console será impresso apenas o Nome, sem o apelido.
+
+  //3. a) No console será impresso tudo o que foi !== de Chijo.
+
+
+  //-------Exercício de escrita
+
+  /* const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+ /* const exerA = ["Lupin", "Polly","Madame","Quentinho", "Fluffy", "Caramelo"
+  ]
+
+  const exerb = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Quentinho", raca: "Salsicha"}
+ ] */
+
+ /* const getPromocao = 'Você ganhou um cupom de desconto, 10% para tosar o/a !'
+ const poodles = pets.filter((pets) => pets.raca === "poodle");
+ const promocao = poodles.map(poodle => promocao(poodle.nome));
+
+ console.log(promocao); */
+
+ 
