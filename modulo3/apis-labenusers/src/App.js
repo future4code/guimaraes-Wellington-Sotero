24f7618c280c5c97372e6ce5314db74ef34956db
wellington <wellingtonmarcelinosotero@hotmail.com>
2022-02-25 20:11:38 -0300

    alterações

--- modulo3/apis-labenusers/src/App.js
+++ modulo3/apis-labenusers/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import cadastro from '.components/cadastro.jsx';
+import paginaUsuario from '.components/paginaUsuario.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá Mundo</h1>
+      <p>Começando a entender fora da Labenu</p>
     </div>
   );
 }
