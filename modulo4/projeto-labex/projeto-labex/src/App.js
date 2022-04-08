1c6a595b0e8117ab9a67ec4e2ff225ffc0b9b2e6
wellington <wellingtonmarcelinosotero@hotmail.com>
2022-04-07 22:15:39 -0300

    projeto-labex

--- modulo4/projeto-labex/projeto-labex/src/App.js
+++ modulo4/projeto-labex/projeto-labex/src/App.js
@@ -1,22 +1,15 @@
-import React from "react";
-import Router from "./Router/Router.js";
-import { MainContainer, GlobalStyle, theme } from "./styled";
-import Footer from "./components/Footer/Footer";
-import { MuiThemeProvider } from "@material-ui/core";
+import './App.css';
+import Router from './rout/router';
 
 
-
-const App = () => {
-
+function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <MainContainer>
-        <GlobalStyle />
-        <Router />
-        <Footer />
-      </MainContainer>
-    </MuiThemeProvider>
-  )
+    <div className="app">
+
+      <Router/>
+
+    </div>
+  );
 }
 
 export default App;
--- modulo4/projeto-labex/projeto-labex/src/pages/Error/ErrorPage.js
+++
@@ -1,5 +0,0 @@
-const ErrorPage = () => {
-    return <h1>Erro! Volte e tente novamente</h1>
-}
-
-export default ErrorPage;
