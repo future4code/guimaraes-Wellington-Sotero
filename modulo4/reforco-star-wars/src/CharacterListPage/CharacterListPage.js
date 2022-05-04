920fc4910acd1ad4b77d1326f08c3931adf3b714
wellington <wellingtonmarcelinosotero@hotmail.com>
2022-05-04 20:47:07 -0300

    atualizações

--- modulo4/reforco-star-wars/src/CharacterListPage/CharacterListPage.js
+++ modulo4/reforco-star-wars/src/CharacterListPage/CharacterListPage.js
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { BASE_URL } from '../constants/urls';
 import { CharacterCard } from './style';
+import Fundo from '../assets/fundo starwars.jpg';
+import styled from "styled-components";
+
+
+const Container = styled.div`
+    background-image: url(${Fundo});
+    background-position: center center;
+    
+
+`
 
 
 const CharacterListPage = () => {
@@ -20,19 +30,18 @@ const CharacterListPage = () => {
     }
 
     const characterCards = characterList.map((character, i) => {
-        return 
-        <CharacterCard>
+        return <div>
             <p key={i}>{character.name}</p>
-        </CharacterCard>
+        </div>
         
 
     })
 
     return (
-        <div>
+        <Container>
             <h2>Lista de Personagens</h2>
             {characterCards}
-        </div>
+        </Container>
     )
 
     
