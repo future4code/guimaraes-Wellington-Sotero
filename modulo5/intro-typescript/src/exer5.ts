b4903cc9a412f192020750c57b573943c63a22f4
wellington <wellingtonmarcelinosotero@hotmail.com>
2022-05-26 21:21:04 -0300

    Atualização dos Exercícios

--- modulo5/intro-typescript/src/exer5.ts
+++ modulo5/intro-typescript/src/exer5.ts
@@ -6,7 +6,7 @@ function checaRenovacaoRG(anoAtual:number, anoNascimento:number, anoEmissao:numb
          return tempoCarteira >= 5 ? "passou dos 5 anos precisa renovar" : "ainda não passou os 5 anos"
 
       }else if(idade >= 20 || idade <= 50) {
-         return tempoCarteira >= 10 ? "passou dos 10 anos precisa renovar" : "ainda não passou os 10 anos"
+         return tempoCarteira >= 10 ? "Passou dos 10 anos precisa renovar" : "ainda não passou os 10 anos"
 
       }else if(idade > 50) {
          return tempoCarteira >=15 ? "passou dos 15 anos precisa renovar" : "ainda não passou os 15 anos"
